036ac3560aa6016cb22e0c81b790c879de1839cd
Alex <oleksandr100204@gmail.com>
2026-01-25 03:55:35 +0100

    initialization a project and finished blocks login and sign-up

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "login.html"),
+        bookmarked: resolve(__dirname, "bookmarked.html"),
+        login: resolve(__dirname, "index.html"),
+        movies: resolve(__dirname, "movies.html"),
+        signUp: resolve(__dirname, "sign-up.html"),
+        tvSeries: resolve(__dirname, "tv-series.html"),
+      },
+    },
+  },
+});
